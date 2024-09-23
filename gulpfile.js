88cb21bb2cf29e8b996bea3b7477a75d8610bcb6
Koza4ok <arturkozak.08@gmail.com>
2024-09-23 22:02:59 +0300

    Виправлена структура і розмітка

--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,68 @@
-const gulp = require('gulp'); // Подключаем Gulp
-const fileInclude = require('gulp-file-include');
-const concat = require('gulp-concat'); // Модуль для объединения файлов
-const sass = require('gulp-sass')(require('sass')); // Модуль для компиляции SCSS в CSS
-const cssnano = require('gulp-cssnano'); // Модуль для минификации CSS
-const rename = require('gulp-rename'); // Модуль для переименования файлов
-const uglify = require('gulp-uglify'); // Модуль для минификации JavaScript
-const browserSync = require('browser-sync').create(); // Модуль для синхронизации с браузером
 
-// Таск для компиляции и минификации SCSS
+const gulp = require('gulp'); // Підключаємо Gulp
+const fileInclude = require('gulp-file-include'); // Для включення файлів в HTML
+const concat = require('gulp-concat'); // Для об'єднання кількох JS файлів в один
+const sass = require('gulp-sass')(require('sass')); // Для компіляції SCSS в CSS
+const cssnano = require('gulp-cssnano'); // Для мінімізації CSS
+const rename = require('gulp-rename'); // Для перейменування файлів (додавання суфіксу .min)
+const uglify = require('gulp-uglify'); // Для мінімізації JS файлів
+const browserSync = require('browser-sync').create(); // Для автоматичного оновлення сторінки при зміні файлів
+const imagemin = require('gulp-imagemin');
+
+
+// Таск для компіляції SCSS в CSS
 gulp.task('scss', function () {
     return gulp.src('./app/scss/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cssnano())
+        .pipe(sass().on('error', sass.logError)) // Компіляція SCSS в CSS
+        .pipe(cssnano()) // Мінімізація CSS
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/css'))
-
-
-});
-gulp.task('css', function () {
-    return gulp.src('./app/css/*.css') // Путь к CSS файлам
-        .pipe(gulp.dest('dist/css')) // Путь назначения
-        .on('end', function() {
-            console.log('CSS files copied to dist/css');
-        });
-
+        .pipe(browserSync.stream());
 });
 
-
-// Таск для HTML
+// Таск для обробки HTML
 gulp.task('html', function () {
     return gulp.src('./app/*.html', { allowEmpty: true })
         .pipe(fileInclude({
-            prefix: '@@', // Префикс для включения компонентов
-            basepath: '@file' // Указывает, что искать компоненты нужно относительно текущего файла
+            prefix: '@@', // Префікс для шаблонів
+            basepath: '@file' // Шлях для включення файлів
         }))
         .pipe(gulp.dest('dist'))
         .pipe(browserSync.stream());
 });
 
-
-
-
-// Таск для объединения и минификации JS
+// Таск для обробки JS
 gulp.task('js', function () {
-    return gulp.src('./app/js/*.js') // Берём все файлы .js
-        .pipe(concat('main.js')) // Объединяем их в один файл main.js
-        .pipe(uglify()) // Минифицируем JS
-        .pipe(rename({ suffix: '.min' })) // Переименовываем в main.min.js
-        .pipe(gulp.dest('dist/js')) // Сохраняем в папке dist/js
-        .pipe(browserSync.stream()); // Обновляем браузер при изменениях
+    return gulp.src('./app/js/*.js')
+        .pipe(concat('main.js')) // Об'єднання JS файлів в один файл
+        .pipe(uglify()) // Мінімізація JS
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream());
 });
 
-const imagemin = require('gulp-imagemin');
 
+
+// Таск для обробки зображень
 gulp.task('images', function () {
-    return gulp.src('./app/images/**/*.{jpg,png,gif,svg}',{ encoding: false })
-        .pipe(imagemin()) // Оптимизация изображений
+    return gulp.src('./app/images/**/*.{jpg,png,gif,svg}', { encoding: false })
+        .pipe(imagemin()) // Стиснення зображень
         .pipe(gulp.dest('dist/images'))
-
 });
 
-
-
-// Таск для запуска сервера и слежения за файлами
+// Таск для запуску сервера і спостереження за змінами в файлах
 gulp.task('serve', function () {
     browserSync.init({
-        server: './dist' // Запускаем сервер с папкой dist как корневой
+        server: './dist' // Вказуємо папку, з якої запуститься сервер
     });
 
-    // Слежение за изменениями файлов и выполнение соответствующих тасков
-    gulp.watch('./app/scss/*.scss', gulp.series('scss')); // Слежение за SCSS
-    gulp.watch('./app/js/*.js', gulp.series('js')); // Слежение за JS
-    gulp.watch('./app/*.html', gulp.series('html')); // Слежение за HTML
-    gulp.watch('./app/images/**/*', gulp.series('images')).on('change', browserSync.reload); // Слежение за изображениями
+    // Спостерігаємо за змінами в SCSS, JS, HTML та зображеннях
+    gulp.watch('./app/scss/*.scss', gulp.series('scss')); // При зміні SCSS запускається відповідний таск
+    gulp.watch('./app/js/*.js', gulp.series('js')); // При зміні JS запускається відповідний таск
+    gulp.watch('./app/*.html', gulp.series('html')); // При зміні HTML запускається відповідний таск
+    gulp.watch('./app/images/**/*', gulp.series('images')).on('change', browserSync.reload); // При зміні зображень оновлюється сторінка
 });
 
-// Таск по умолчанию (запускает сборку и сервер)
-gulp.task('default', gulp.series('html', 'scss' ,'css', 'js','images', 'serve'));
 
+gulp.task('default', gulp.series('html', 'scss', 'js', 'images', 'serve'));
 
